2b7c592fa6eba3f007e162fc495376b0029e5e43
tayrone_murilo <tayrone_murilo@SNFNS2EP13VM042.florianopolis.sc.senai.br>
2014-09-04 22:16:12 -0300

    implementado o @test

--- introducaoJPA/src/teste/Main.java
+++ introducaoJPA/src/teste/Main.java
@@ -6,7 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class Principal {
+public class Main {
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence
 				.createEntityManagerFactory("introducao-jpa");
